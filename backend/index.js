27445f3906a4f390d30a92991ef4230ae5b45a5d
Sudhanshu kumar <sudhanshu95084145@gmail.com>
2026-04-17 22:38:23 +0530

    Added API feature

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+// External packages
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+// Internal packages
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", (req, res) => {
+    res.json({name: "Hello, bro"})
+})
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        console.log("Database is successfully connected 😎");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+        })
+    })
+    .catch(err => {
+        console.log('Error while connecting to database', err);
+    })
